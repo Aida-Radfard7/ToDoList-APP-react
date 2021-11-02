a07d79be5bea38bc05fb41554048542bb8d15235
Aida Radfard <aida.radfard.1@gmail.com>
2021-11-02 13:05:32 +0330

    welcome changes

--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -3,12 +3,12 @@ import '../assets/welcome.css';
 import { useSelector } from "react-redux";
 
 
-export const Welcome = () => {
+export const Welcome = ({isHide,isDark}) => {
   const login = useSelector((store) => store.loginState);
 
     return (
-        <section className="main mt-5">
-            <section className="welcome">
+        <section className={isHide ? "main mt-5 hide-sideBar" :"main mt-5"}>
+            <section className={isDark[1] ? "dark-welcome" :"welcome"}>
                 <section className="welcome-img"></section>
                 <h2 className="welcoming-text">Welcome {login[0].username}</h2>
             </section>

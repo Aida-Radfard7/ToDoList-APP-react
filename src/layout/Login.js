57eedc6ad30a5798c8cd5275a63aa3259596c943
Aida Radfard <aida.radfard.1@gmail.com>
2021-11-12 17:05:58 +0330

    set default value for inputs in login page

--- src/layout/Login.js
+++ src/layout/Login.js
@@ -1,12 +1,13 @@
 import React ,{useRef} from 'react'
 import '../assets/login.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch , useSelector } from 'react-redux';
 import {setLogin} from '../state-management/actions/loginAction'
 import { useHistory , Link} from 'react-router-dom';
 
 
 export const Login = () => {
 
+    const login = useSelector((store) => store.loginState);
     const dispatch = useDispatch();
     const history = useHistory();
     const username = useRef();
@@ -25,8 +26,8 @@ export const Login = () => {
         <section className="login">
             <form className="form" onSubmit={submit}  >
                     <h2>Login</h2>
-                    <input type="text" placeholder="Enter Your Name ..." ref={username} />
-                    <input type="email" placeholder="Enter Your Email ..." ref={email} />  
+                    <input type="text" placeholder="Enter Your Name ..." ref={username} defaultValue={login[0].username} />
+                    <input type="email" placeholder="Enter Your Email ..." ref={email} defaultValue={login[0].email} />  
                     <input type="submit" /> 
             </form>
             <section className="back">

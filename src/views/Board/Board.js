0324c31d2beecfad4158be570829f88710ac8da7
aida radfard <aida.radfard.1@gmail.com>
2022-09-14 21:22:50 +0430

    bug:lists & addNewList were in conflict (stupid sulotion)

--- src/views/Board/Board.js
+++ src/views/Board/Board.js
@@ -1,74 +1,17 @@
-import { React, useState , useRef } from "react";
+import { React } from "react";
 import "../../assets/board.css";
-import { useSelector, useDispatch } from "react-redux";
-import { boardListAdd } from "../../state-management/actions/boardListAction";
+import { useSelector } from "react-redux";
 import { Lists } from "./Lists";
-import { v4 as uuidv4 } from 'uuid';
 
 
 export const Board = ({isHide}) => {
 
-  const [click, setClick] = useState(false);
-  const boardList = useSelector(store => store.boardListState);
   const dark = useSelector(store => store.themeState);
-  const dispatch = useDispatch();
-  const listName = useRef();
-
-  const AddList = (newList , uuid) =>{
-    if(newList != ""){
-        dispatch(boardListAdd(newList , uuid)) 
-        listName.current.value= "";
-        listName.current.focus();       
-    }
-  }
 
   return (
       <section className={isHide ? "main mt-5 board-sidebar-hide" :"main mt-5"}>
         <section className="board-content">
           <Lists isDark={dark[1]} />
-          {click ? (
-            <section className="addList-section">
-              <input
-                ref={listName}
-                id="input-List"
-                className="p-2 border-0"
-                maxLength="30"
-                data-placeholder="Enter List Name ..."
-              />
-              <section className="addList-section-btn mt-2 mb-3">
-                <button
-                  onClick={() => AddList(listName.current.value , uuidv4())}
-                  id="addBtn"
-                  className=" px-3"
-                >
-                  Add
-                </button>
-                <button
-                  onClick={() => setClick(false)}
-                  id="cancelBtn"
-                  className=" px-2"
-                >
-                  Cancel
-                </button>
-              </section>
-            </section>
-          ) : (
-            <a
-              onClick={() => setClick(true)}
-              className="link"
-            >
-              <section className="add-new-list">
-                <ul>
-                  <li className="mx-2">
-                    <i className=" fa fa-plus"></i>
-                  </li>
-                  <li>
-                    <a>Add a New List</a>
-                  </li>
-                </ul>
-              </section>
-            </a>
-          )}
         </section>
       </section>
   );
--- src/views/Board/Lists.js
+++ src/views/Board/Lists.js
@@ -8,6 +8,8 @@ import {Cards} from './Cards';
 import { v4 as uuidv4 } from 'uuid';
 import $ from "jquery";
 import 'jquery-ui/ui/widgets/sortable';
+import { boardListAdd } from "../../state-management/actions/boardListAction";
+
 
 
 
@@ -66,6 +68,17 @@ export const Lists = ({isDark}) => {
         setIndex(index);
     }
 
+    const dark = useSelector(store => store.themeState);
+    const listName = useRef();
+  
+    const AddList = (newList , uuid) =>{
+      if(newList != ""){
+          dispatch(boardListAdd(newList , uuid)) 
+          listName.current.value= "";
+          listName.current.focus();       
+      }
+    }
+  
 
     return (
         <section id={"sortable2"} className="lists connectedSortable2">
@@ -97,6 +110,52 @@ export const Lists = ({isDark}) => {
                     </section>
                 </section>
                     )}
+
+                    <section className="addList-section">
+                    {click ? (
+                        <section className='addList-form'>
+                        <input
+                            ref={listName}
+                            id="input-List"
+                            className="p-2 border-0"
+                            maxLength="30"
+                            data-placeholder="Enter List Name ..."
+                        />
+                        <section className="addList-section-btn mt-2 mb-3">
+                            <button
+                            onClick={() => AddList(listName.current.value , uuidv4())}
+                            id="addBtn"
+                            className=" px-3"
+                            >
+                            Add
+                            </button>
+                            <button
+                            onClick={() => setClick(false)}
+                            id="cancelBtn"
+                            className=" px-2"
+                            >
+                            Cancel
+                            </button>
+                        </section>
+                        </section>
+                    ) : (
+                        <a
+                        onClick={() => setClick(true)}
+                        className="link"
+                        >
+                        <section className="add-new-list">
+                            <ul>
+                            <li className="mx-2">
+                                <i className=" fa fa-plus"></i>
+                            </li>
+                            <li>
+                                <a>Add a New List</a>
+                            </li>
+                            </ul>
+                        </section>
+                        </a>
+                    )}
+                    </section>
         </section>
     )
 }
